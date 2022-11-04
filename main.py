c9f32c8d1c054c71ab77be6851e2febf917415d0
I-am-a-smartie <63035352+I-am-a-smartie@users.noreply.github.com>
2022-11-04 18:46:18 +0530

    move left and right
    seal is finally moving left and right!!!!

--- main.py
+++ main.py
@@ -1,67 +1,125 @@
+import math
+
 import pygame
 
 WIDTH, HEIGHT = 956, 546
 DEFAULT_IMAGE_SIZE = (100, 100)
 
-IMGS = [pygame.image.load('seal.png')]
+IMGS = [pygame.transform.rotate(pygame.transform.scale(pygame.image.load('seal.png'), DEFAULT_IMAGE_SIZE), 45)]
+
 BG_IMG = pygame.image.load('kai-bg2.png')
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Kai")
 
-
+def blit_rotate_center(win, image, top_left, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(
+        center=image.get_rect(topleft=top_left).center)
+    win.blit(rotated_image, new_rect.topleft)
 
 FPS = 120
+PADDLE_HEIGHT = 40
+PADDLE_WIDTH = 10
+MAX_VEL = 5
 
-class Seal:
-    VEL = 5
+class AbstractSeal:
+    def __init__(self, max_vel, rotation_vel):
+        self.img = self.IMG
+        self.max_vel = max_vel
+        self.vel = 0
+        self.angle = 0
+        self.rotation_vel = rotation_vel
+        self.x, self.y = self.START_POS
+        self.acceleration = 0.1
 
-    def __init__(self, x, y, width, height, color):
-        self.img = None
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
 
     def draw(self, win):
-        #pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-        self.img = pygame.transform.rotate(pygame.transform.scale(IMGS[0], DEFAULT_IMAGE_SIZE), 45)
-        win.blit(self.img, (WIDTH/2-50, HEIGHT-200))
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
 
-def draw(win, paddle):
-    paddle.draw(win)
+    def move_left(self):
+        self.vel = min(self.vel + self.acceleration, self.max_vel)
+        self.move()
+
+    def move_right(self):
+        self.vel = min(self.vel + self.acceleration, self.max_vel)
+        self.x -= self.vel
+    def move(self):
+        self.x += self.vel
+
+class PlayerSeal(AbstractSeal):
+    IMG = IMGS[0]
+    START_POS = (0, HEIGHT-150)
+
+def draw(win, player_seal):
+    player_seal.draw(win)
     pygame.display.update()
+
 
 def main():
     clock = pygame.time.Clock()
+    player_seal = PlayerSeal(4,4)
 
-    seal = Seal(WIDTH / 2, HEIGHT / 2, 10, 10, 'black')
-
-    i = 0
     run = True
     while run:
-        # win.fill((0,0,0))
         win.blit(BG_IMG, (0,0))
-        # win.blit(BG_IMG, (WIDTH+i, 0))
-        #
-        # if i == -WIDTH:
-        #     win.blit(BG_IMG, (WIDTH+i, 0))
-        #     i = 0
-        # i -= 10
-
         clock.tick(FPS)
-
+        draw(win, player_seal)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
-        draw(win, seal)
+        keys = pygame.key.get_pressed()
+        moved = False
+
+        if keys[pygame.K_d]:
+            moved = True
+            player_seal.move_left()
+        if keys[pygame.K_a]:
+            moved = True
+            player_seal.move_right()
+        if keys[pygame.K_r]:
+            player_seal.rotate(right=True)
+
+        draw(win, player_seal)
 
     pygame.quit()
     quit()
 
 if __name__ == "__main__":
     main()
+
+
+# class Seal:
+#     VEL = 5
+#
+#     def __init__(self, x, y, width, height, color):
+#         self.img = None
+#         self.x = x
+#         self.y = y
+#         self.width = width
+#         self.height = height
+#         self.color = color
+#         self.vel = 0
+#         self.max_vel = MAX_VEL
+#         self.acceleration = 0.1
+#
+#     def draw(self, win):
+#         #self.img = pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+#         self.img = pygame.transform.rotate(pygame.transform.scale(IMGS[0], DEFAULT_IMAGE_SIZE), 45)
+#         win.blit(self.img, (WIDTH/2-50, HEIGHT-200))
+#
+#     def move_forward(self):
+#         self.vel = min(self.vel + self.acceleration, self.max_vel)
+#         self.move()
+#
+#
+#     def move(self):
+#         self.x += self.vel
 
