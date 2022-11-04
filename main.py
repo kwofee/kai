fe68041debb3d32baa39f906d898938a72115728
I-am-a-smartie <63035352+I-am-a-smartie@users.noreply.github.com>
2022-11-04 12:18:23 +0530

    Update main.py
    seal not moving, try rotate function from car racer tutorial tech with tim in utils py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import math
+
 import pygame
 
 WIDTH, HEIGHT = 956, 546
 DEFAULT_IMAGE_SIZE = (100, 100)
 
-IMGS = [pygame.image.load('seal.png')]
+IMGS = [pygame.transform.rotate(pygame.transform.scale(pygame.image.load('seal.png'), DEFAULT_IMAGE_SIZE), 45)]
+
 BG_IMG = pygame.image.load('kai-bg2.png')
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -12,44 +15,53 @@ pygame.display.set_caption("Kai")
 
 
 FPS = 120
+PADDLE_HEIGHT = 40
+PADDLE_WIDTH = 10
+MAX_VEL = 5
 
-class Seal:
-    VEL = 5
-
-    def __init__(self, x, y, width, height, color):
-        self.img = None
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
+class AbstractSeal:
+    def __init__(self, max_vel):
+        self.img = self.IMG
+        self.max_vel = max_vel
+        self.vel = 0
+        self.angle = 0
+        self.x, self.y = self.START_POS
+        self.acceleration = 0.1
 
     def draw(self, win):
-        #pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-        self.img = pygame.transform.rotate(pygame.transform.scale(IMGS[0], DEFAULT_IMAGE_SIZE), 45)
-        win.blit(self.img, (WIDTH/2-50, HEIGHT-200))
+        win.blit(self.img, (0,0))
 
-def draw(win, paddle):
-    paddle.draw(win)
+    def move_forward(self):
+        self.vel = min(self.vel + self.acceleration, self.max_vel)
+        self.move()
+
+    def move(self):
+        radians = math.radians(self.angle)
+        vertical = math.cos(radians)*self.vel
+        horizontal = math.sin(radians) * self.vel
+
+        self.y -= vertical
+        self.x -= horizontal
+
+class PlayerSeal(AbstractSeal):
+    IMG = IMGS[0]
+    START_POS = (180, 200)
+
+def draw(win, player_seal):
+    win.blit(IMGS[0], (0,0))
+
+    player_seal.draw(win)
     pygame.display.update()
+
 
 def main():
     clock = pygame.time.Clock()
 
-    seal = Seal(WIDTH / 2, HEIGHT / 2, 10, 10, 'black')
+    player_seal = PlayerSeal(4)
 
-    i = 0
     run = True
     while run:
-        # win.fill((0,0,0))
         win.blit(BG_IMG, (0,0))
-        # win.blit(BG_IMG, (WIDTH+i, 0))
-        #
-        # if i == -WIDTH:
-        #     win.blit(BG_IMG, (WIDTH+i, 0))
-        #     i = 0
-        # i -= 10
-
         clock.tick(FPS)
 
         for event in pygame.event.get():
@@ -57,11 +69,46 @@ def main():
                 run = False
                 break
 
-        draw(win, seal)
+        keys = pygame.key.get_pressed()
+        moved = False
+
+        if keys[pygame.K_d]:
+            moved = True
+            player_seal.move_forward()
+
+        draw(win, player_seal)
 
     pygame.quit()
     quit()
 
 if __name__ == "__main__":
     main()
+
+
+# class Seal:
+#     VEL = 5
+#
+#     def __init__(self, x, y, width, height, color):
+#         self.img = None
+#         self.x = x
+#         self.y = y
+#         self.width = width
+#         self.height = height
+#         self.color = color
+#         self.vel = 0
+#         self.max_vel = MAX_VEL
+#         self.acceleration = 0.1
+#
+#     def draw(self, win):
+#         #self.img = pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+#         self.img = pygame.transform.rotate(pygame.transform.scale(IMGS[0], DEFAULT_IMAGE_SIZE), 45)
+#         win.blit(self.img, (WIDTH/2-50, HEIGHT-200))
+#
+#     def move_forward(self):
+#         self.vel = min(self.vel + self.acceleration, self.max_vel)
+#         self.move()
+#
+#
+#     def move(self):
+#         self.x += self.vel
 
