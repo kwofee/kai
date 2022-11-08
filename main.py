d9d18a709b4cf0f1f8e94a243318111bf52cd466
I-am-a-smartie <63035352+I-am-a-smartie@users.noreply.github.com>
2022-11-08 09:13:03 +0530

    seagull and ball
    seagull is added in 5 different columns, change value of columns when difficulty changes

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import math
+import random
 
 import pygame
 
 WIDTH, HEIGHT = 956, 546
 DEFAULT_IMAGE_SIZE = (100, 100)
+NUM_OF_GULLS = 5
 
-IMGS = [pygame.transform.rotate(pygame.transform.scale(pygame.image.load('seal.png'), DEFAULT_IMAGE_SIZE), 45)]
+IMGS = [
+    pygame.transform.rotate(pygame.transform.scale(pygame.image.load('seal.png'), DEFAULT_IMAGE_SIZE), 45),
+    pygame.transform.scale(pygame.image.load('seagull.png'), DEFAULT_IMAGE_SIZE),
+    pygame.transform.scale(pygame.image.load('beach-ball.png'), (50, 50))
+]
 
 BG_IMG = pygame.image.load('kai-bg2.png')
 
@@ -21,7 +27,7 @@ def blit_rotate_center(win, image, top_left, angle):
 FPS = 120
 PADDLE_HEIGHT = 40
 PADDLE_WIDTH = 10
-MAX_VEL = 5
+MAX_VEL = 100
 
 class AbstractSeal:
     def __init__(self, max_vel, rotation_vel):
@@ -31,7 +37,7 @@ class AbstractSeal:
         self.angle = 0
         self.rotation_vel = rotation_vel
         self.x, self.y = self.START_POS
-        self.acceleration = 0.1
+        self.acceleration = 20
 
     def rotate(self, left=False, right=False):
         if left:
@@ -51,25 +57,71 @@ class AbstractSeal:
         self.x -= self.vel
     def move(self):
         self.x += self.vel
+        self.collide_edge()
+
+    def collide_edge(self):
+        if self.x <= 0:
+            self.x = WIDTH/2 - 50
+        elif self.x + IMGS[0].get_width() >= WIDTH:
+            self.x = WIDTH/2 - 50
+
 
 class PlayerSeal(AbstractSeal):
     IMG = IMGS[0]
-    START_POS = (0, HEIGHT-150)
+    START_POS = (WIDTH/2 - 50, HEIGHT-150)
 
-def draw(win, player_seal):
+class Gull:
+    def __init__(self):
+        self.img = IMGS[1]
+        self.num = NUM_OF_GULLS
+        # self.x = random.randrange(self.img.get_width(), WIDTH-self.img.get_width())
+        self.y = random.randrange(self.img.get_height(), HEIGHT-400)
+        self.x = 0
+
+    def draw(self, win):
+        win.blit(self.img, (self.x, self.y))
+
+
+class Ball:
+    def __init__(self):
+        self.img = IMGS[2]
+        self.x = 0
+        self.y = 0
+
+    def draw(self, win):
+        win.blit(self.img, (self.x, self.y))
+
+def draw(win, player_seal, gulls, ball):
     player_seal.draw(win)
+    for i in gulls:
+        i.draw(win)
+    ball.draw(win)
     pygame.display.update()
+
+
 
 
 def main():
     clock = pygame.time.Clock()
     player_seal = PlayerSeal(4,4)
+    ball = Ball()
+    gulls = []
+    x = [0, 159, 318, 477, 636]
+    y = [159, 318, 477, 636, 956]
+    d = [(0, 159), (159, 318), (318, 477), (477, 636), (636,956)]
+
+    for i,j in d:
+        gull = Gull()
+        gull.x = random.randrange(i, j)
+        print(gull.x)
+        gulls.append(gull)
+
 
     run = True
     while run:
         win.blit(BG_IMG, (0,0))
         clock.tick(FPS)
-        draw(win, player_seal)
+        draw(win, player_seal, gulls, ball)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -87,7 +139,7 @@ def main():
         if keys[pygame.K_r]:
             player_seal.rotate(right=True)
 
-        draw(win, player_seal)
+        draw(win, player_seal, gulls, ball)
 
     pygame.quit()
     quit()
